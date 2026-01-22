a41393ce0a6c2c9530c66e150f4801620247e1bb
Bruh-Codes <kamasahdickson20@gmail.com>
2026-01-22 16:59:24 +0000

    add javascript week 1

---
+++ javascript-week1/myscript.js
@@ -0,0 +1,27 @@
+let firstName = "Alice";
+let lastName = "Johnson";
+let city = "New York";
+
+let age = 25;
+let siblings = 2;
+let pets = 1;
+
+let height = 5.7;
+let weight = 135.5;
+let temperature = 98.6;
+
+let isStudent = true;
+let hasLicense = false;
+let isEmployed = true;
+
+document.write(isStudent);
+document.write(hasLicense);
+document.write(isEmployed);
+
+document.write(height);
+document.write(weight);
+document.write(temperature);
+
+document.write(age);
+document.write(siblings);
+document.write(pets);
